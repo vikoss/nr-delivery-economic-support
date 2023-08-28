38e9067ffc4fbd82ca02ab6ce3fd82bfbf35ae09
vikoss <vikoss.yiss@gmail.com>
2023-08-27 23:43:49 -0600

    feat: add vuetify

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <v-combobox
+      clearable
+      label="Combobox"
+      :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+    />
   </div>
 </template>
