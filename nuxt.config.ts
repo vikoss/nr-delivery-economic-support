70a51ac96633d24d199e54f17ed24f9c61568a93
vikoss <vikoss.yiss@gmail.com>
2023-08-28 13:22:15 -0600

    feat: add generator of tickets and counters

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: false,
   css: ['vuetify/lib/styles/main.sass'],
   build: {
     transpile: ['vuetify'],
   },
+  modules: ['@sidebase/nuxt-pdf'],
 })
